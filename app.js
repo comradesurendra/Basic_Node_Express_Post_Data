90b4bd9ee123841d7e6eb6f1bf3899929e095aea
comradesurendra <surendrakumarjuet@gmail.com>
2020-06-07 23:47:16 +0530

    semifinal commit

--- app.js
+++ app.js
@@ -10,25 +10,27 @@ app.set('view engine','handlebars');
 app.use(bodyparser.urlencoded({extended:true}));
 
 app.get('/',(req,res)=>{
-    res.render('index',{title :'Home Page'});
+    res.render('registration',{title :'Home Page'});
 });
 
-app.post('/',(req,res)=>{
-
-    let firstname=req.body.first;
-    let lastname=req.body.last;
-    let emailid=req.body.email;
-
-    // res.write();
-    // res.write();
-    // res.write();
-
-    res.send("<h5>First Name : </h5>"+ firstname +"<br>"+ "<h5>Last Name : </h5>"+ lastname +"<br>"+ "<h5>Email id : </h5>"+ emailid );
+app.get('/handleform',(req,res)=>{
+    res.render('handleform',{
+            title :'Form Data',
+            'data':{
+            'name':req.query.name,
+            'email':req.query.email,
+            'password':req.query.password,
+            'phone':req.query.phone
+    }});
 });
 
-app.get('/about',(req,res)=>{
-    res.render('about',{title:'About Page'});
+app.post('/handleform',(req,res)=>{
+   res.render('handleform',{
+    title :'Form Data',
+       data:req.body
+   })
 });
+
 
 app.listen('8080',()=>{
     console.log("Server started on port 8080.");
